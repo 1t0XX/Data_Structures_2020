da0c7575b7150fd4f0602ed93b9ed9ba304bfd01
1t0XX <53831070+1t0XX@users.noreply.github.com>
2021-01-24 11:43:03 +0300

    DZ8

---
+++ src/Lesson8/HashTable.java
@@ -0,0 +1,25 @@
+package Lesson8;
+
+public interface HashTable<K, V> extends ICollection<T> {
+
+    boolean put(K key, V value);
+
+    V get(K key);
+
+    boolean contains(K key);
+
+    V remove(K key);
+
+    void display();
+
+
+    interface Entry<K, V> {
+        K getKey();
+        void setKey(K key);
+
+        V getValue();
+        void setValue(V value);
+    }
+
+
+}
